2881fdaff12bc4851afabb3ce27b998a3babc198
Tyler Madsen <tylermadsen@mail.weber.edu>
2017-02-28 17:11:55 +0000

    here's the file

---
+++ Mancester_United_hw6_task1.c
@@ -0,0 +1,31 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  Mancester_United_hw6_task1.c
+ *       	Usage:  ./Mancester_United_hw6_task1.c
+ *    Description:  convert Rectangular to polar
+ *
+ *        Version:  1.0
+ *        Created:  02/28/2017 05:10:22 PM
+ *       Compiler:  gcc -Wall -Werror
+ *         Author:  Tyler Madsen (), tylermadsen@mail.weber.edu
+ * =====================================================================================
+ */
+#include <stdio.h>		/* For Standard I/O */
+#include <stdlib.h>
+#include <time.h>
+#include <mathlib.h>
+
+/* Function Prototypes */
+
+/* Main Program */
+int main(int argc, char *argv[])
+{
+
+	return 0;
+}
+
+
+/* Function Defenitions */
+
+
